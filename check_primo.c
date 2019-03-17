87be54a117de3b84b59156059e4dd3f6bd574af8
GuhAlex <gustavo.gub@hotmail.com>
2019-03-17 12:07:11 -0300

    Código simples para verificar se o número digitado é primo

---
+++ check_primo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show(void);
+void scan(void);
+void check(void);
+
+// Global variables
+int num;
+int divi = 0;
+int count = 0;
+
+int main(){
+
+  system("clear || cls");
+  show();
+  scan();
+  check();
+
+  return 0;
+}
+
+void show(void)
+
+{
+  printf("\t\t\tDigite um número inteiro:\n\t\t\t");
+}
+
+void scan(void)
+
+{
+  scanf("%i", &num);
+  if (num == 1)
+  {
+    printf("\t\t\tO número %i não é primo\n", num);
+    exit(0);
+  }
+  while (divi < num)
+  {
+    ++divi;
+  if (divi > 1 && divi < num && num % divi == 0) //Números primos ão divisiveis somente por um e por ele mesmo
+    ++count;
+   }
+}
+
+void check()
+
+{
+ if (count >= 1)
+   printf("\t\t\tO número %i não é primo \n", num);
+
+ else
+   printf("\t\tO número %i é primo \n", num);
+}
